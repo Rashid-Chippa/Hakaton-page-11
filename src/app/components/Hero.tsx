e29fa2cffa56a81eaa2092c76bc42db51e26703a
Rashid-Chippa <=>
2024-12-08 20:05:45 +0500

    first commit

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,213 @@
+'use client'
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+<section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="flex flex-wrap -m-4">
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-64 rounded overflow-hidden">
+          <img
+            alt="ecommerce"
+            className="object-cover object-center w-full h-full block"
+            src="/chef1.png"
+          />
+        </a>
+        <div className="mt-4">
+          
+          <h2 className="text-gray-900 title-font text-lg font-medium">
+          Tehmina Rumi
+          </h2>
+          <p className="mt-1">Chef</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-64 rounded overflow-hidden">
+          <img
+            alt="ecommerce"
+            className="object-cover object-center w-full h-full block"
+            src="/chef2.png"
+          />
+        </a>
+        <div className="mt-4">
+          
+          <h2 className="text-gray-900 title-font text-lg font-medium">
+            Jorina Begum
+          </h2>
+          <p className="mt-1">Chef</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-64 rounded overflow-hidden">
+          <img
+            alt="ecommerce"
+            className="object-cover object-center w-full h-full block"
+            src="/chef3.png"
+          />
+        </a>
+        <div className="mt-4">
+          
+          <h2 className="text-gray-900 title-font text-lg font-medium">
+            
+            M.Muhammad
+          </h2>
+          <p className="mt-1">Chef</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-64 rounded overflow-hidden">
+          <img
+            alt="ecommerce"
+            className="object-cover object-center w-full h-full block"
+            src="/chef4.png"
+          />
+        </a>
+        <div className="mt-4">
+          
+          <h2 className="text-gray-900 title-font text-lg font-medium">
+          Munna Kathy
+          </h2>
+          <p className="mt-1">Chef</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-64 rounded overflow-hidden">
+          <img
+            alt="ecommerce"
+            className="object-cover object-center w-full h-full block"
+            src="/chef5.png"
+          />
+        </a>
+        <div className="mt-4">
+          
+          <h2 className="text-gray-900 title-font text-lg font-medium">
+            Themidha Rumi
+          </h2>
+          <p className="mt-1">Chef</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-64 rounded overflow-hidden">
+          <img
+            alt="ecommerce"
+            className="object-cover object-center w-full h-full block"
+            src="/chef6.png"
+          />
+        </a>
+        <div className="mt-4">
+          
+          <h2 className="text-gray-900 title-font text-lg font-medium">
+            Bisnu Devgon
+          </h2>
+          <p className="mt-1">Chef</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-64 rounded overflow-hidden">
+          <img
+            alt="ecommerce"
+            className="object-cover object-center w-full h-full block"
+            src="/chef7.png"
+          />
+        </a>
+        <div className="mt-4">
+         
+          <h2 className="text-gray-900 title-font text-lg font-medium">
+            Moin Molladasf
+          </h2>
+          <p className="mt-1">Chef</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-64 rounded overflow-hidden">
+          <img
+            alt="ecommerce"
+            className="object-cover object-center w-full h-full block"
+            src="/chef8.png"
+          />
+        </a>
+        <div className="mt-4">
+          
+          <h2 className="text-gray-900 title-font text-lg font-medium">
+            William Rumi
+          </h2>
+          <p className="mt-1">Chef</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-64 rounded overflow-hidden">
+          <img
+            alt="ecommerce"
+            className="object-cover object-center w-full h-full block"
+            src="/chef9.png"
+          />
+        </a>
+        <div className="mt-4">
+          
+          <h2 className="text-gray-900 title-font text-lg font-medium">
+          Kets William Roy
+          </h2>
+          <p className="mt-1">Chef</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-64 rounded overflow-hidden">
+          <img
+            alt="ecommerce"
+            className="object-cover object-center w-full h-full block"
+            src="/chef10.png"
+          />
+        </a>
+        <div className="mt-4">
+          
+          <h2 className="text-gray-900 title-font text-lg font-medium">
+          Muhammad Kholil
+          </h2>
+          <p className="mt-1">Chef</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-64 rounded overflow-hidden">
+          <img
+            alt="ecommerce"
+            className="object-cover object-center w-full h-full block"
+            src="/chef11.png"
+          />
+        </a>
+        <div className="mt-4">
+          
+          <h2 className="text-gray-900 title-font text-lg font-medium">
+          Ataur Rehman
+          </h2>
+          <p className="mt-1">Chef</p>
+        </div>
+      </div>
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-64 rounded overflow-hidden">
+          <img
+            alt="ecommerce"
+            className="object-cover object-center w-full h-full block"
+            src="/chef12.png"
+          />
+        </a>
+        <div className="mt-4">
+          
+          <h2 className="text-gray-900 title-font text-lg font-medium">
+          Monalisa Holly
+          </h2>
+          <p className="mt-1">Chef</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+    </div>
+  )
+}
+
+export default Hero
+
